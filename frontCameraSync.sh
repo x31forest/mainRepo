e0dab22f0f57bc94b5bb84434ff6bf4cbd4a4f92
x31forest <x31forest@gmail.com>
2018-01-16 22:56:45 -0500

    Create frontCameraSync.sh

---
+++ frontCameraSync.sh
@@ -0,0 +1,19 @@
+echo Syncing folders
+
+syncFiles=$(find /volume1/surveillance/Front -mtime 0 -type f -printf '%T@\t%p\n' | sort -t $'\t' -g| cut -d $'\t' -f 2-)
+for syncFile in $syncFiles
+do
+    echo -n Transferring $syncFile
+    destFile=${syncFile/\/volume1/\/volume1\/CloudSync}
+    echo to $destFile
+    mkdir -p $(dirname "$destFile")
+    cp -uv "${syncFile}" "${destFile}"
+done
+
+echo Removing extra files
+find /volume1/CloudSync/surveillance/Front -type f -printf '%T@\t%p\n' |
+grep mp4 |
+sort -t $'\t' -g |
+head -n -288 |
+cut -d $'\t' -f 2- |
+xargs rm -rf {}
