289828a07f86105d5077c9878924b2336112f2ae
x31forest <x31forest@gmail.com>
2018-01-16 23:07:19 -0500

    Update backCameraSync.sh

--- synology/cloudSync/backCameraSync.sh
+++ synology/cloudSync/backCameraSync.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+# Files recorded by Synology Surveillance app arrive in /volume1/surveillance/Back
+# Files are copied to CloudSync directory that is syncronized with google Drive. 
+# We only want the last 2 days available on google drive due to space constraints
+# 
 echo Syncing folders
 
+# Grab all files modified today
 syncFiles=$(find /volume1/surveillance/Back -mtime 0 -type f -printf '%T@\t%p\n' | sort -t $'\t' -g| cut -d $'\t' -f 2-)
+
+# Transfer each file to cloudsync if does not exist, create directory structure
 for syncFile in $syncFiles
 do
-        echo -n Transferring $syncFile
+    echo -n Transferring $syncFile
     destFile=${syncFile/\/volume1/\/volume1\/CloudSync}
     echo to $destFile
     mkdir -p $(dirname "$destFile")
@@ -11,6 +19,7 @@ do
 done
 
 echo Removing extra files
+# one file every 10 min, and we want to keep 2 days = 288 files
 find /volume1/CloudSync/surveillance/Back -type f -printf '%T@\t%p\n' |
 grep mp4 |
 sort -t $'\t' -g |
